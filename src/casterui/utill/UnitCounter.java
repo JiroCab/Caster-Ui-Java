f0229fcc5d27189d0e9495f39d2c72455ddc53fb
JiroCab <Jirocab.89@gmail.com>
2023-04-28 19:22:45 +0800

    Unit counter porotype

--- src/casterui/utill/UnitCounter.java
+++ src/casterui/utill/UnitCounter.java
@@ -1,33 +1,20 @@
 package casterui.utill;
 
-import casterui.CuiVars;
 import mindustry.game.Team;
-import mindustry.gen.Groups;
-import mindustry.gen.Unit;
 import mindustry.type.UnitType;
 
-import java.util.HashMap;
-
 public class UnitCounter {
-    public Iterable<Unit> unitsIterator = Groups.unit;
-    public HashMap<Integer, UnitType> top = new HashMap<>();
+    public Integer amount = 0;
+    public UnitType type = null;
+    public Team team = Team.get(0);
 
-    public  void count(Unit unit){
-        if (unit.spawnedByCore && CuiVars.showCoreUnits) return;
-    }
-
-    public int amount = 0;
-    public UnitType type;
-    public Team team;
-
-    public void setAmount(Integer amount){
-        this.amount = amount;
-    }
-
-    UnitCounter(Team team, UnitType type, Integer amount){
+    public UnitCounter(Integer amount, UnitType type, Team team){
         this.amount = amount;
         this.type = type;
         this.team = team;
+    }
+    public  UnitCounter(Integer amount, UnitType type, Integer team){
+        new UnitCounter(amount, type, Team.get(team));
     }
 
 }
