a92b94d4d4861e9e8ee74796b69707cfd412d410
JiroCab <Jirocab.89@gmail.com>
2023-05-20 18:09:06 +0800

    Self contained hotkeys & Polish
    + Hotkeys is now in the Caster UI instead of regular hotkeys
    + Alert Styles + Finer speed adjustment
    + Hotkey Options to respect various scenarios
    + UnitPlayerTable Separate per teams units
    ~ UnitPlayerTable can now be customized to hide certain units
    ~ Build.gradle mindustryVersion = 'v143.1' -> 'v144.1'
    ~ Mod.hjson polish & version bump
    ~ More Polish in Settings
    ~ Bug Fix: Spectators not shown even with option to hide them are off
    ~ Bug Fix: Crash related to Core circle alerts
    ~ Bug Fix: PlayerUnitTable not Being fully hidden while turn off mid game
    ~ Updated: Readme.md

--- src/casterui/CuiVars.java
+++ src/casterui/CuiVars.java
@@ -3,12 +3,10 @@ package casterui;
 import arc.Core;
 import arc.util.Log;
 import arc.util.Time;
-import casterui.io.CuiBinding;
 import casterui.io.CuiInputs;
 import casterui.io.ui.CuiFragment;
 import casterui.io.ui.CuiWorldRenderer;
-import casterui.io.ui.dialog.CuiSettingsDialog;
-import casterui.io.ui.dialog.CuiTeamMangerDialog;
+import casterui.io.ui.dialog.*;
 import mindustry.Vars;
 import mindustry.gen.Player;
 import mindustry.gen.Unit;
@@ -22,20 +20,20 @@ public class CuiVars {
     public static CuiFragment fragment = new CuiFragment();
     public static CuiInputs inputs = new CuiInputs();
     public static CuiTeamMangerDialog teamManger = new CuiTeamMangerDialog();
+    public static CuiRebindDialog rebindDialog = new CuiRebindDialog();
 
-    public static boolean showCoreUnits = true, initialized = false, unitTableCollapse = true, fastUpdate = false;
+    public static boolean showCoreUnits = true, initialized = false, unitTableCollapse = true, fastUpdate = false, drawRally = false;
     public static Player hoveredPlayer, clickedPlayer;
     public static Unit heldUnit, hoveredEntity, clickedEntity;
     public static float  timer = 0, nextUpdate = 100, nextUpdateFast = 50;
     public static Tile lastCoreDestroyEvent;
-    public static int updateDelay = Core.settings.getInt("cui-unitsPlayerTableUpdateRate");
     public static Map<Integer, Player> mappedPlayers = new HashMap<>();
 
 
     public static void init(){
-        CuiBinding.load();
         CuiSettingsDialog.buildCategory();
         renderer.worldRenderer();
+        rebindDialog.load();
         Log.info("Caster user interface loaded! happy casting!! owo");
     }
 
@@ -46,8 +44,7 @@ public class CuiVars {
         CuiVars.heldUnit = null;
         initialized = false;
         lastCoreDestroyEvent = null;
-        nextUpdate = timer + (updateDelay * 5);
-        updateDelay = Core.settings.getInt("cui-unitsPlayerTableUpdateRate");
+        nextUpdate = timer + (Core.settings.getInt("cui-unitsPlayerTableUpdateRate") * 5);
         fragment.BuildTables(Vars.ui.hudGroup);
     }
 
@@ -58,11 +55,11 @@ public class CuiVars {
         timer = Time.globalTime;
         if(Vars.state.isPlaying() && !initialized) {
             if (timer >= nextUpdate ) {
-                nextUpdate = timer + (updateDelay * 5);
+                nextUpdate = timer + (Core.settings.getInt("cui-unitsPlayerTableUpdateRate") * 5);
 
                 if(!fastUpdate)fragment.StutteredUpdateTables();
             } else if (timer >= nextUpdateFast ){
-                nextUpdateFast = timer + (updateDelay * 2);
+                nextUpdateFast = timer + (Core.settings.getInt("cui-unitsPlayerTableUpdateRate") * 2);
                 fragment.slowUpdateTables();
                 if(fastUpdate)fragment.StutteredUpdateTables();
             }
--- src/casterui/io/ui/CuiFragment.java
+++ src/casterui/io/ui/CuiFragment.java
@@ -9,6 +9,7 @@ import arc.scene.ui.Label;
 import arc.scene.ui.layout.Table;
 import arc.util.Scaling;
 import casterui.CuiVars;
+import casterui.io.ui.dialog.CuiSettingsDialog;
 import mindustry.Vars;
 import mindustry.game.Team;
 import mindustry.gen.*;
@@ -22,6 +23,7 @@ import mindustry.world.blocks.units.UnitFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static arc.Core.settings;
@@ -39,7 +41,7 @@ public class CuiFragment {
             playerIconSize = Core.settings.getInt("cui-playerIconSize"),
             unitsIconSize = Core.settings.getInt("cui-unitsIconSize");
     float buttonSize;
-    public Boolean  unitTableCompactPlayers = false, showBlockTable = false;
+    public Boolean  unitTableCompactPlayers = false, showBlockTable = false, showTableUnitsPlayer = false;
     public Building mouseBuilding = null;
     public int iconSizes = 25;
 
@@ -56,7 +58,7 @@ public class CuiFragment {
             if(Core.settings.getBool("cui-ShowPlayerList")) unitPlayerTable.add(playersTable).left();
             if(Core.settings.getBool("cui-ShowPlayerList") && Core.settings.getBool("cui-ShowUnitTable")) unitPlayerTable.row();
             if(Core.settings.getBool("cui-ShowUnitTable")) unitPlayerTable.add(unitTable).left();
-            parentCont.add(unitPlayerTable).visible(() -> CuiVars.unitTableCollapse);
+            parentCont.add(unitPlayerTable).visible(() -> CuiVars.unitTableCollapse && showTableUnitsPlayer);
         });
 
         if(Core.settings.getBool("cui-ShowBlockInfo")){
@@ -73,6 +75,7 @@ public class CuiFragment {
     public void StutteredUpdateTables() {
         if (CuiVars.hoveredEntity != null && !unitPlayerTable.hasMouse()) CuiVars.hoveredEntity = null;
         buttonSize = (float) Core.settings.getInt("cui-buttonSize");
+        showTableUnitsPlayer = (Core.settings.getBool("cui-ShowPlayerList") || Core.settings.getBool("cui-ShowUnitTable"));
 
         // region Control table
         controlTable.clear();
@@ -86,28 +89,41 @@ public class CuiFragment {
         unitTable.clear();
         if (Core.settings.getBool("cui-ShowUnitTable")) {
             AtomicInteger icons = new AtomicInteger();
+            //prevent  doubling of rows
+            AtomicBoolean newRow = new AtomicBoolean(false);
 
             for (int id = 0 ; id < Team.all.length ; id++){
                 Team team = Team.get(id);
-                if(settings.getBool("cui-separateTeamsUnit"))unitTable.row();
                 if(team.data().units.size >= 1) {
+                    if(settings.getBool("cui-separateTeamsUnit") && !newRow.get()){
+                        if(!newRow.get())icons.set(0);
+                        unitTable.row();
+                    }
+
                     Map<Short, Integer> teamUnits = new HashMap<>();
 
                     for (Unit u : team.data().units.sort(unit -> unit.type.id)) {
+                        if(CuiSettingsDialog.hiddenUnits.contains(u.type) && CuiVars.showCoreUnits ) break;
+                        if(!CuiVars.showCoreUnits && CuiSettingsDialog.coreUnitsTypes.contains(u.type))break;
+                        if(!CuiVars.showCoreUnits && u.spawnedByCore && settings.getBool("cui-unitFlagCoreUnitHides"))break;
+
                         teamUnits.merge(u.type.id, 1, Integer::sum);
                     }
                     teamUnits.forEach((u, i ) ->{
                         UnitType unit = Vars.content.unit(u);
-                        unitTable.image(new TextureRegion(unit.fullIcon)).tooltip(unit.localizedName).size(unitsIconSize).get();
+
+                        unitTable.image(new TextureRegion(unit.fullIcon)).tooltip(unit.localizedName).size(unitsIconSize).scaling(Scaling.bounded).get();
                         unitTable.add(new Label(() -> "[#" + team.color.toString() + "]" + i + "[white]")).get();
 
                         if (icons.get() >= settings.getInt("cui-unitsPlayerTableSize")) {
                             unitTable.row();
                             icons.set(0);
-                        } else icons.getAndIncrement();
+                            newRow.set(true);
+                        } else {
+                            icons.getAndIncrement();
+                            newRow.set(false);
+                        }
                     });
-
-
                 }
             }
         }
@@ -120,7 +136,7 @@ public class CuiFragment {
 
             Groups.player.each(player -> {
                 if(player == Vars.player) return;
-                if(settings.getBool("cui-hideNoUnitPlayers") && (player.unit() == null) || !player.team().data().hasCore())return;
+                if(settings.getBool("cui-hideNoUnitPlayers") && (player.unit() == null) && !player.team().data().hasCore())return;
 
                 Label teamIcon = new Label(() -> player.team().emoji.equals("") ? "[#" + player.team().color + "]" +player.team().id + "[]" : player.team().emoji);
                 if (!unitTableCompactPlayers) playersTable.add(teamIcon).with( w -> w.tapped( () -> setTrackPlayer(player)));
@@ -142,6 +158,7 @@ public class CuiFragment {
     }
 
     public void slowUpdateTables(){
+        //region Block info slow
         /*Moved here to reduce flickering*/
         blockItemTable.clear();
         blockLiquidTable.clear();
@@ -182,6 +199,7 @@ public class CuiFragment {
             Vec2 mouse = Core.input.mouseWorld(Core.input.mouseX(), Core.input.mouseY());
             Tile mouseTile = Vars.world.tileWorld(mouse.x, mouse.y);
             showBlockTable = false;
+            CuiVars.drawRally = false;
 
             mouseBuilding = mouseTile != null ? mouseTile.build : null;
             if (mouseBuilding != null) { //the less cool deltanedas/waisa
@@ -197,22 +215,26 @@ public class CuiFragment {
                     String sign = power > 0 ? "[stat]+" : "[red]";
 
                     blockTable.label(() -> Core.bundle.get("cui-block-info.power") + ": "+  sign + power).row();
-                    if(mouseBuilding.block instanceof Battery || mouseBuilding.block instanceof PowerNode || mouseBuilding.block instanceof BeamNode) blockTable.label(() -> "[stat]"+ Math.round(mouseBuilding.sense(LAccess.powerNetStored))+ "[white]/[accent]" + Math.round(mouseBuilding.sense(LAccess.powerCapacity)));
+                    if(mouseBuilding.block instanceof Battery || mouseBuilding.block instanceof PowerNode || mouseBuilding.block instanceof BeamNode) blockTable.label(() -> "[stat]"+ Math.round(mouseBuilding.sense(LAccess.powerNetStored))+ "[white]/[accent]" + Math.round(mouseBuilding.sense(LAccess.powerNetCapacity)));
                 }
                 if (mouseBuilding.items != null && mouseBuilding.items.total() > 0) blockTable.add(blockItemTable).row();
                 if (mouseBuilding.liquids != null) blockTable.add(blockLiquidTable).row();
 
                 if(mouseBuilding.config() instanceof String)blockTable.label(() -> mouseBuilding.config().toString()).row();
+                /* why bother being fancy and reading directly from the block if have logic for that */
                 if(mouseBuilding.block instanceof UnitFactory && mouseBuilding.senseObject(LAccess.config)!= null){
                     blockTable.table(a-> {
                         a.label(() -> Vars.content.unit(mouseBuilding.senseObject(LAccess.config).toString()).localizedName).pad(1f);
                         a.image(() -> Vars.content.unit(mouseBuilding.senseObject(LAccess.config).toString()).fullIcon).size(iconSizes).pad(1f).row();
                     }).row();
 
-                    if(mouseBuilding.getCommandPosition() != null) blockTable.label(() -> Core.bundle.get("cui-block-info.rally") + ": " + Math.round(mouseBuilding.getCommandPosition().x) + ", "+ Math.round(mouseBuilding.getCommandPosition().y)).row();
+                    if(mouseBuilding.getCommandPosition() != null){
+                        blockTable.label(() -> Core.bundle.get("cui-block-info.rally") + ": " + Math.round(mouseBuilding.getCommandPosition().x) + ", "+ Math.round(mouseBuilding.getCommandPosition().y)).row();
+                        CuiVars.drawRally = true;
+                    }
                 }
                 if(mouseBuilding.block instanceof Turret)blockTable.label(() -> "[accent]"+  mouseBuilding.sense(LAccess.ammo) + "[white]/[orange]"+ ((Turret) mouseBuilding.block).maxAmmo).row();
-                //TODO: heat, block constructors
+                //TODO: heat, block constructors, Payload
 
             }
         }
